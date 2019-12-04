56752b166dc070700921149dfb1efa7b057446cb
tianyu <tianyu5177@sina.com>
2019-12-04 16:13:55 +0800

    init

--- 1_原生ajax/server.js
+++ 1_原生ajax/server.js
@@ -2,19 +2,19 @@
 const express = require('express')
 //实例一个app应用对象
 const app = express()
-//使用内置中间件去解析post请求参数
+//使用内置中间件去解析post请求中以urlencoded形式编码的参数
 app.use(express.urlencoded({extended:true}))
 //暴露静态资源
 app.use(express.static(__dirname+'/public'))
 
 app.get('/test_get',(req,res)=>{
   console.log(req.query)
-  res.send('ok')
+  res.send('你发来的是get请求，我收到了，这是给你的数据：哈哈?????')
 })
 
 app.post('/test_post',(req,res)=>{
   console.log(req.body);
-  res.send('ok')
+  res.send('你发来的是post请求，我收到了，这是给你的数据：哈哈')
 })
 
 app.listen(3000,(err)=>{
@@ -22,5 +22,6 @@ app.listen(3000,(err)=>{
   else {
     console.log('兄弟不要不用编译器打开页面')
     console.log('测试原生js发送ajax_get请求的地址是：http://localhost:3000/ajax_get.html')
+    console.log('测试原生js发送ajax_post请求的地址是：http://localhost:3000/ajax_post.html')
   }
 })
